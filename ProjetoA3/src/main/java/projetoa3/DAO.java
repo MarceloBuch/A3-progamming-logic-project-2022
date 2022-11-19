584e72212a837506efb35f34b9f30d8f5866ad9b
Marcelo Buchalowicz <marcelo.buch@hotmail.com>
2022-11-19 13:57:31 -0300

    update DAO.java

--- ProjetoA3/src/main/java/projetoa3/DAO.java
+++ ProjetoA3/src/main/java/projetoa3/DAO.java
@@ -5,15 +5,27 @@ import java.sql.ResultSet;
 
 public class DAO {
     public boolean existe(Usuario usuario) throws Exception{
-        String sql="SELECT * FROM tb_usuario WHERE nome=? AND senha=?";
+        String sql="SELECT * FROM tb_usuario WHERE Nome_Usuario=? AND Senha_Usuario=?";
         try(Connection conn=ConexaoDB.obterConexao()){
                 PreparedStatement ps = conn.prepareStatement(sql);
                     ps.setString(1,usuario.getNomeUsuario());
-                    ps.setString(2,usuario.getSenha());
+                    ps.setString(2,usuario.getSenhaUsuario());
                     try(ResultSet rs = ps.executeQuery()){
                         return rs.next();
                     }        
         }              
+    }
+    
+    public void CadastrarUsuario(Usuario usuario) throws Exception{
+        String sql = "INSERT INTO tb_usuario(IDUsuario, Nome_Usuario, Senha_Usuario, CPF_Usuario, Tipo_Usuario) values(default, ?, ?, ?, 0)";
+        try(Connection conn = ConexaoDB.obterConexao()){
+            PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setString(1, usuario.getNomeUsuario());
+                ps.setString(2, usuario.getSenhaUsuario());
+                ps.setString(3, usuario.getCPFUsuario());
+                ps.execute(); 
+        }
+    
     }
     
 }
